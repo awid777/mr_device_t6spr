264d77cd89bd0d0b093a91564e8b0ec0bef25816
Austin Widner <austinw125@gmail.com>
2014-05-30 10:45:23 -0500

    update for htc one max (sprint)

--- mr_init_devices.c
+++ mr_init_devices.c
@@ -14,9 +14,9 @@ const char *mr_init_devices[] =
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p33",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p37",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p35",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p42",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p39",
 
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
@@ -29,8 +29,8 @@ const char *mr_init_devices[] =
 
     // for adb
     "/sys/class/tty/ptmx",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p35", //system
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p36", // /cache
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p39", //system
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p41", // /cache
     "/sys/class/misc/android_adb",
     "/sys/class/android_usb/android0/f_adb",
     "/sys/bus/usb",
